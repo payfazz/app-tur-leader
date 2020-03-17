ceef0125f4227ea151f88080d0a3719c74820f17
andisultan <andi.m.sultan@gmail.com>
2020-03-17 17:31:19 +0700

    add app

---
+++ precache-manifest.cf506a953078de48e49c0590cb2b5d8d.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9e355bb4d7e1ac9346a9e5c7433656",
+    "url": "/app-tur-leader/index.html"
+  },
+  {
+    "revision": "0a7d260e50109f0c8812",
+    "url": "/app-tur-leader/static/css/0.8bf4b66c.chunk.css"
+  },
+  {
+    "revision": "12a9a5b2d2818ca707fc",
+    "url": "/app-tur-leader/static/css/10.2e5a9296.chunk.css"
+  },
+  {
+    "revision": "5089b5d5097f81bc0956",
+    "url": "/app-tur-leader/static/css/11.2e5a9296.chunk.css"
+  },
+  {
+    "revision": "4c8b9fdbff585e0aee7d",
+    "url": "/app-tur-leader/static/css/12.2e5a9296.chunk.css"
+  },
+  {
+    "revision": "ce28be3bfb682373067a",
+    "url": "/app-tur-leader/static/css/4.8bf4b66c.chunk.css"
+  },
+  {
+    "revision": "7d65be95c4366af2dfc7",
+    "url": "/app-tur-leader/static/css/5.8bf4b66c.chunk.css"
+  },
+  {
+    "revision": "a19afac2c80e89533fbb",
+    "url": "/app-tur-leader/static/css/6.8bf4b66c.chunk.css"
+  },
+  {
+    "revision": "521d554d68eb89fcc4fc",
+    "url": "/app-tur-leader/static/css/7.c261a07a.chunk.css"
+  },
+  {
+    "revision": "5ef973e52222b9afc271",
+    "url": "/app-tur-leader/static/css/8.5bbbf1da.chunk.css"
+  },
+  {
+    "revision": "536e39e43bf2eb3586e1",
+    "url": "/app-tur-leader/static/css/9.2e5a9296.chunk.css"
+  },
+  {
+    "revision": "fbf6f3ca2069e0035ded",
+    "url": "/app-tur-leader/static/css/main.69805e99.chunk.css"
+  },
+  {
+    "revision": "0a7d260e50109f0c8812",
+    "url": "/app-tur-leader/static/js/0.02e58cbd.chunk.js"
+  },
+  {
+    "revision": "12a9a5b2d2818ca707fc",
+    "url": "/app-tur-leader/static/js/10.30e9fb44.chunk.js"
+  },
+  {
+    "revision": "5089b5d5097f81bc0956",
+    "url": "/app-tur-leader/static/js/11.0ad74ec2.chunk.js"
+  },
+  {
+    "revision": "4c8b9fdbff585e0aee7d",
+    "url": "/app-tur-leader/static/js/12.c1ef9c02.chunk.js"
+  },
+  {
+    "revision": "2978bce7e8a8085cfa67",
+    "url": "/app-tur-leader/static/js/3.dd62efda.chunk.js"
+  },
+  {
+    "revision": "4780b8b2de2be387f6f795439b687178",
+    "url": "/app-tur-leader/static/js/3.dd62efda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce28be3bfb682373067a",
+    "url": "/app-tur-leader/static/js/4.5aa475a5.chunk.js"
+  },
+  {
+    "revision": "7d65be95c4366af2dfc7",
+    "url": "/app-tur-leader/static/js/5.740c577d.chunk.js"
+  },
+  {
+    "revision": "a19afac2c80e89533fbb",
+    "url": "/app-tur-leader/static/js/6.92fe3061.chunk.js"
+  },
+  {
+    "revision": "521d554d68eb89fcc4fc",
+    "url": "/app-tur-leader/static/js/7.3f859c7b.chunk.js"
+  },
+  {
+    "revision": "5ef973e52222b9afc271",
+    "url": "/app-tur-leader/static/js/8.5d126401.chunk.js"
+  },
+  {
+    "revision": "536e39e43bf2eb3586e1",
+    "url": "/app-tur-leader/static/js/9.43cfb404.chunk.js"
+  },
+  {
+    "revision": "fbf6f3ca2069e0035ded",
+    "url": "/app-tur-leader/static/js/main.4fe28976.chunk.js"
+  },
+  {
+    "revision": "bad2196baec03b59a773",
+    "url": "/app-tur-leader/static/js/runtime-main.6d28f8c8.js"
+  },
+  {
+    "revision": "024030908618c2f40ae9a1c120a4a92f",
+    "url": "/app-tur-leader/static/media/FazzNeue-Bold.02403090.woff2"
+  },
+  {
+    "revision": "64c9117475f764f675e0e6449aaa46bf",
+    "url": "/app-tur-leader/static/media/FazzNeue-Bold.64c91174.eot"
+  },
+  {
+    "revision": "a239159a28cee6fe3a4c0df87a9c6cca",
+    "url": "/app-tur-leader/static/media/FazzNeue-Bold.a239159a.ttf"
+  },
+  {
+    "revision": "dadb08771417b144684e7a3e6d8344ff",
+    "url": "/app-tur-leader/static/media/FazzNeue-Bold.dadb0877.woff"
+  },
+  {
+    "revision": "18b16b542d721ca85eae593ec1d69841",
+    "url": "/app-tur-leader/static/media/FazzNeue-BoldItalic.18b16b54.eot"
+  },
+  {
+    "revision": "7a20473e6d32bbb71ccd9d7873560c5c",
+    "url": "/app-tur-leader/static/media/FazzNeue-BoldItalic.7a20473e.woff"
+  },
+  {
+    "revision": "84b7d8853a1a524aca6748975d1ac6d0",
+    "url": "/app-tur-leader/static/media/FazzNeue-BoldItalic.84b7d885.woff2"
+  },
+  {
+    "revision": "ddc335821133878dbe968b1f08a0c366",
+    "url": "/app-tur-leader/static/media/FazzNeue-BoldItalic.ddc33582.ttf"
+  },
+  {
+    "revision": "3228dce777667059811e6fc1f9bb37bc",
+    "url": "/app-tur-leader/static/media/FazzNeue-Heavy.3228dce7.ttf"
+  },
+  {
+    "revision": "8c1c503ca0ac918df1b7aac6e39cf8e1",
+    "url": "/app-tur-leader/static/media/FazzNeue-Heavy.8c1c503c.woff"
+  },
+  {
+    "revision": "bbdcf46d4549a2aae371bc4a46c076ed",
+    "url": "/app-tur-leader/static/media/FazzNeue-Heavy.bbdcf46d.woff2"
+  },
+  {
+    "revision": "f644f50c288c9f75c591c91a8f4a3413",
+    "url": "/app-tur-leader/static/media/FazzNeue-Heavy.f644f50c.eot"
+  },
+  {
+    "revision": "5210f1a766df674f0b978cd881eb1bdf",
+    "url": "/app-tur-leader/static/media/FazzNeue-HeavyItalic.5210f1a7.eot"
+  },
+  {
+    "revision": "741d4306f1bafa616e9844bde0185536",
+    "url": "/app-tur-leader/static/media/FazzNeue-HeavyItalic.741d4306.woff"
+  },
+  {
+    "revision": "973610ef3aaf9d414d1a27f8a608ea42",
+    "url": "/app-tur-leader/static/media/FazzNeue-HeavyItalic.973610ef.ttf"
+  },
+  {
+    "revision": "bdc7e237ef1bc14bd91723b5fb30edc7",
+    "url": "/app-tur-leader/static/media/FazzNeue-HeavyItalic.bdc7e237.woff2"
+  },
+  {
+    "revision": "4e3935b9cf441ff44248839eb6515d55",
+    "url": "/app-tur-leader/static/media/FazzNeue-Italic.4e3935b9.woff2"
+  },
+  {
+    "revision": "4f3be30f8be80358a39660d44be723e4",
+    "url": "/app-tur-leader/static/media/FazzNeue-Italic.4f3be30f.woff"
+  },
+  {
+    "revision": "68091e51e4551ddfb3929bf539af5d18",
+    "url": "/app-tur-leader/static/media/FazzNeue-Italic.68091e51.ttf"
+  },
+  {
+    "revision": "82cef51b584fdfafaeba990aac1b7f04",
+    "url": "/app-tur-leader/static/media/FazzNeue-Italic.82cef51b.eot"
+  },
+  {
+    "revision": "125cccb54db0f71b5b2b034453293b8c",
+    "url": "/app-tur-leader/static/media/FazzNeue-Light.125cccb5.woff"
+  },
+  {
+    "revision": "3025015df09f618b75d3c0c702aad8cb",
+    "url": "/app-tur-leader/static/media/FazzNeue-Light.3025015d.ttf"
+  },
+  {
+    "revision": "35240a5d4388870f83901f45cff0b260",
+    "url": "/app-tur-leader/static/media/FazzNeue-Light.35240a5d.eot"
+  },
+  {
+    "revision": "43047d5235ca32d07da45000a87d669d",
+    "url": "/app-tur-leader/static/media/FazzNeue-Light.43047d52.woff2"
+  },
+  {
+    "revision": "046e545062da39df4770f4febc673161",
+    "url": "/app-tur-leader/static/media/FazzNeue-LightItalic.046e5450.woff"
+  },
+  {
+    "revision": "17b706da116fce7ec4b2a3eee02392c8",
+    "url": "/app-tur-leader/static/media/FazzNeue-LightItalic.17b706da.woff2"
+  },
+  {
+    "revision": "3acfc645630fd35cf77811eda3e457ed",
+    "url": "/app-tur-leader/static/media/FazzNeue-LightItalic.3acfc645.eot"
+  },
+  {
+    "revision": "d9bf651f513bfbab1f7aa92eec881b16",
+    "url": "/app-tur-leader/static/media/FazzNeue-LightItalic.d9bf651f.ttf"
+  },
+  {
+    "revision": "49dea202d2767b97f5bc04f241844869",
+    "url": "/app-tur-leader/static/media/FazzNeue-Medium.49dea202.woff"
+  },
+  {
+    "revision": "6cfb79a60bba20d6c076865d0b0e7b3b",
+    "url": "/app-tur-leader/static/media/FazzNeue-Medium.6cfb79a6.woff2"
+  },
+  {
+    "revision": "c39b14f7271368439dd11abf17026800",
+    "url": "/app-tur-leader/static/media/FazzNeue-Medium.c39b14f7.eot"
+  },
+  {
+    "revision": "d46299c64260956d497a5a35e272f708",
+    "url": "/app-tur-leader/static/media/FazzNeue-Medium.d46299c6.ttf"
+  },
+  {
+    "revision": "20b105ced0356ad484c386373c481306",
+    "url": "/app-tur-leader/static/media/FazzNeue-MediumItalic.20b105ce.ttf"
+  },
+  {
+    "revision": "ab57c9a74d4867233e5ba945a28e43d6",
+    "url": "/app-tur-leader/static/media/FazzNeue-MediumItalic.ab57c9a7.woff2"
+  },
+  {
+    "revision": "c28db59b5fd68264c1ed34986abade50",
+    "url": "/app-tur-leader/static/media/FazzNeue-MediumItalic.c28db59b.woff"
+  },
+  {
+    "revision": "f705b0b674b0a96a9c8acb751c382bc4",
+    "url": "/app-tur-leader/static/media/FazzNeue-MediumItalic.f705b0b6.eot"
+  },
+  {
+    "revision": "088fced4a2f8668c3f8fbc2c0434e1bf",
+    "url": "/app-tur-leader/static/media/FazzNeue-Regular.088fced4.ttf"
+  },
+  {
+    "revision": "55917c13f2f453a3949b09e5b75109b9",
+    "url": "/app-tur-leader/static/media/FazzNeue-Regular.55917c13.woff2"
+  },
+  {
+    "revision": "9a5efa3d137efa8c7d93e9379900f221",
+    "url": "/app-tur-leader/static/media/FazzNeue-Regular.9a5efa3d.eot"
+  },
+  {
+    "revision": "ca2b10c1dac573939838a82dd148f93f",
+    "url": "/app-tur-leader/static/media/FazzNeue-Regular.ca2b10c1.woff"
+  },
+  {
+    "revision": "3c73c416b0b0f43d281c62e5f2d9935b",
+    "url": "/app-tur-leader/static/media/fontello.3c73c416.eot"
+  },
+  {
+    "revision": "8d9e6642363103bae4515fb48543802d",
+    "url": "/app-tur-leader/static/media/fontello.8d9e6642.svg"
+  },
+  {
+    "revision": "9fd1b6548e40c393d0a5ced029941c75",
+    "url": "/app-tur-leader/static/media/fontello.9fd1b654.woff2"
+  },
+  {
+    "revision": "b60ed2dbb4c3f5a7201f9c5f245709a1",
+    "url": "/app-tur-leader/static/media/fontello.b60ed2db.woff"
+  },
+  {
+    "revision": "e2d967684e9bd7fcaae14cc3ed0dbaf6",
+    "url": "/app-tur-leader/static/media/fontello.e2d96768.ttf"
+  }
+]);
